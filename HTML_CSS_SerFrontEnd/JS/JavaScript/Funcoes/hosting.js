64055b9005a4741f08516675e3aa0b627b0fb8dc
Betotal <48997213+Betotal@users.noreply.github.com>
2025-04-29 07:13:19 -0300

    JS1

---
+++ HTML_CSS_SerFrontEnd/JS/JavaScript/Funcoes/hosting.js
@@ -0,0 +1,14 @@
+"use strict"
+teste()
+function teste(){
+    console.log("Teste hosting")
+}
+
+teste1()
+
+const teste2 = function teste1(){
+    console.log("Teste de hosting")
+}
+
+let minhavar = "Teste"
+console.log(minhavar)
