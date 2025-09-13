8898e2313051f8b082402d61f8523dccb43a0e8b
Betotal <48997213+Betotal@users.noreply.github.com>
2025-09-13 20:22:20 -0300

    Scroll

--- HTML_CSS_SerFrontEnd/JS/Date/Aniversario/js/birthday.js
+++ HTML_CSS_SerFrontEnd/JS/Date/Aniversario/js/birthday.js
@@ -1,4 +1,20 @@
-function quantoFaltaPara(m, d) {
+function quantoFaltaPara(mes, dia) {
 
+        const hoje = new Date();
+        hoje.setHours(0,0,0,0)
 
+        let anoAtual = hoje.getFullYear();
+        let aniversario = new Date(anoAtual, mes - 1, dia);
+
+        const hojeMili      = +hoje        // hoje.getTime()  // transformar em milisegundos
+        let aniversarioMili = +aniversario // aniversario.getTime()  // transformar em milisegundos
+
+        console.log(hojeMili, aniversarioMili)
+        if (aniversarioMili < hojeMili) {
+            aniversario.setFullYear(++anoAtual);
+            aniversarioMili = aniversario.getTime();
+        }
+
+        const umDiaMili = 1000 * 60 * 60 * 24;
+        return Math.floor((aniversarioMili - hojeMili) / umDiaMili);
 }
--- HTML_CSS_SerFrontEnd/JS/Date/SaveTheDate/js/savethedate.js
+++ HTML_CSS_SerFrontEnd/JS/Date/SaveTheDate/js/savethedate.js
@@ -1,36 +1,40 @@
 ;(function () {
     'use strict'
 
-    const dateEventDOM = document.querySelector(".hero-content h1 span").innerText
-    console.log(dateEventDOM)
-    
-    const dateEvent = getDate(dateEventDOM)
-
-    let distancia = dateEvent.getTime() - (new Date())
-
     const seg = 1000
     const min = 60 * seg
     const hora = 60 * min
     const dia = 24 * hora
+
+    const dateEventDOM = document.querySelector(".hero-content h1 span").innerText
+    console.log(dateEventDOM)    
     
-    const diasFaltam = Math.floor(distancia / dia)
+    const p = document.createElement("p")
+    document.querySelector(".hero-content").appendChild(p)
+
+    function updateCountdown() {
+         const dateEvent = getDate(dateEventDOM)
+        let distancia = dateEvent.getTime() - (new Date())
+
+        const diasFaltam = Math.floor(distancia / dia)
     
-    distancia = distancia - diasFaltam * dia
-    const horasFaltam = Math.floor(distancia / hora)
+        distancia = distancia - diasFaltam * dia
+        const horasFaltam = Math.floor(distancia / hora)
 
-    distancia = distancia - horasFaltam * hora
-    const minFaltam = Math.floor(distancia / min)
+        distancia = distancia - horasFaltam * hora
+        const minFaltam = Math.floor(distancia / min)
 
-    distancia = distancia - minFaltam * min
-
-    const segFaltam = Math.floor(distancia / 1000)
-    addContagem(diasFaltam, horasFaltam, minFaltam, segFaltam )
+        distancia = distancia - minFaltam * min
+        const segFaltam = Math.floor(distancia / 1000)
+        
+        addContagem(diasFaltam, horasFaltam, minFaltam, segFaltam )
+    }
 
     function addContagem(day,hour,min, sec){
-        const p = document.createElement("p")
-        p.textContent = `Contagem Regressiva: ${day} dias, ${hour} horas, ${min} e ${sec} segundos.`
-        document.querySelector(".hero-content").appendChild(p)
+        p.textContent = `Contagem Regressiva: ${day} dias, ${hour} horas, ${min} minutos e ${sec} segundos.`
     }
+
+    setInterval(updateCountdown, 1000)
 
     function getDate(str) {
         const [date, hour] = str.split(" ")
